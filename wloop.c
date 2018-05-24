fc9f1b9d0941b06313e7fda00a87894cc1cb94d6
natchan241 <channatalee@gmail.com>
2018-05-24 21:50:15 +0000

    Iteration

---
+++ wloop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+
+  while (a < 5)
+  {
+    printf("a is equal to %d\n", a);
+    a++;
+  }
+  printf("done - a is equal to %d\n", a);
+
+  for (int i = 0; i < 100; i++)
+  {
+    if((i % 5) == 0)
+    {
+      printf("%d\n", i);
+    }
+  }
+	  
+}
